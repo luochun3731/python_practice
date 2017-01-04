85a1130acc062e1254151e75ea4cbed02f5c9947
Luo Chun <luochun@myhome.163.com>
2017-01-04 11:28:00 +0800

    edit

--- design_patterns/factory_method.py
+++ design_patterns/factory_method.py
@@ -1,5 +1,5 @@
 '''
-Factory Method
+Factory Method: 不通过类而是通过工厂函数来创建对象.
 '''
 
 import xml.etree.ElementTree as etree
@@ -12,6 +12,7 @@ class JSONConnector:
         with open(file_path, mode='r', encoding='utf-8') as f:
             self.data = json.load(f)
 
+    # @Properyty将成员函数的调用变成属性赋值（负责把一个方法变成属性调用）
     @property
     def parsed_data(self):
         return self.data
@@ -21,6 +22,7 @@ class XMLConnector:
     def __init__(self, file_path):
         self.tree = etree.parse(file_path)
 
+    # @Properyty将成员函数的调用变成属性赋值（负责把一个方法变成属性调用）
     @property
     def parsed_data(self):
         return self.tree
@@ -47,11 +49,11 @@ def connect_to(file_path):
 
 def main():
     sqlite_factory = connect_to('data/person.sq3')
-    print()
 
     xml_factory = connect_to('data/person.xml')
     xml_data = xml_factory.parsed_data
     liars = xml_data.findall(".//{}[{}='{}']".format('person', 'lastName', 'Liar'))
+    print('#################### xml data ####################')
     print('found: {} persons'.format(len(liars)))
     for liar in liars:
         print('first name: {}'.format(liar.find('firstName').text))
@@ -61,10 +63,12 @@ def main():
 
     json_factory = connect_to('data/donut.json')
     json_data = json_factory.parsed_data
+    print('#################### json data ####################')
     for donut in json_data:
         print('name: {}'.format(donut['name']))
         print('price: {}'.format(donut['ppu']))
         [print('toppint: {} {}'.format(t['id'], t['type'])) for t in donut['topping']]
+        print('\n')
 
 
 if __name__ == '__main__':
